eea0b7e5bf1c6448580b4da90966cd69317c7d8d
doge33 <sandylx33@gmail.com>
2020-08-26 23:14:21 +0000

    tried auto-move up, but commented out; also added name of player

--- client.js
+++ client.js
@@ -2,23 +2,32 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.222.131',// ip??
-    port: 50542// port?
+    host: '10.0.2.15',// ip?? //135.23.222.131 for public
+    port: 50541// port? //50542 for public
   });
   //interpret incoming data as text
   conn.setEncoding('utf8');
 
   conn.on('connect', () => {  //print sth as soon as connection established
     console.log("Yayy! Successfully connected to game server~");
-    conn.write("Name: DDD");
-
-  });
-
+    conn.write("Name: 333");
+    conn.write("Move: up");
+    
+    /*
+    let moved=0;
+    const move = setInterval(()=> {
+      conn.write("Move: up");
+      moved++;
+      moved === 5 ? clearInterval(move): '';
+    }, 200);
+    */
+    
   conn.on('data', (data) => {
     console.log(data);
   })
 
   return conn;
+})
 }
 
 module.exports = {connect};  //if you want it as an object shorthand, export it as object
