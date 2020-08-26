02115ae694bdd962696705e91740bb359445db93
doge33 <sandylx33@gmail.com>
2020-08-26 20:09:35 +0000

    modified client.js to notify successful connection and send name info to server

--- client.js
+++ client.js
@@ -8,6 +8,12 @@ const connect = function() {
   //interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {  //print sth as soon as connection established
+    console.log("Yayy! Successfully connected to game server~");
+    conn.write("Name: DDD");
+
+  });
+
   conn.on('data', (data) => {
     console.log(data);
   })
