2b044d1f6a925034ae407182c6050faae239d745
doge33 <sandylx33@gmail.com>
2020-08-26 19:55:24 +0000

    created client.js to contain the connect function and export it to play.js as object shorthand

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',// ip??
+    port: 50542// port?
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = {connect};  //if you want it as an object shorthand, export it as object
--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const net = require('net');
+//const net = require('net');
 //establishes connection with the game server
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',// ip??
-    port: 50542// port?
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting...');
 
