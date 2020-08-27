2587d356f0f300805a03999a05301f0b883e3e8c
doge33 <sandylx33@gmail.com>
2020-08-27 00:45:39 +0000

    separated to input.js to solely handle user input

---
+++ input.js
@@ -0,0 +1,26 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  const handleUserInput = (key) => {
+    if (key === '\u0003'){
+      process.exit()
+    }
+  }
+  stdin.on('data', (key)=>{
+    handleUserInput(key)
+  });
+
+  return stdin;
+}
+
+
+module.exports ={ setupInput };
--- play.js
+++ play.js
@@ -1,34 +1,13 @@
-//const net = require('net');
 //establishes connection with the game server
 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting...');
 
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  const handleUserInput = (key) => {
-    if (key === '\u0003'){
-      process.exit()
-    }
-  }
-  stdin.on('data', (key)=>{
-    handleUserInput(key)
-  });
-
-  return stdin;
-}
 
 setupInput();
 
